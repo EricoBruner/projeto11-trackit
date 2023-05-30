ab03fde1a1918b3b22656d3ede6b3d4725c78ef2
EricoBruner <ericodasilvabruner@gmail.com>
2023-05-30 18:31:23 -0300

    adicionado sistema de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
 function App() {
   return (
-    <>
-      {/* <Login /> */}
-      <Register />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
